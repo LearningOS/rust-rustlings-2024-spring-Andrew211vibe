b1e627b76b604b9353cd7e243aaccce6b03a9551
Andrew211vibe <1207368051@qq.com>
2024-04-09 01:23:19 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -11,7 +11,7 @@ use std::mem;
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-
+    let _ = my_option.is_some();
     let my_arr = &[
         -1, -2, -3,
         -4, -5, -6
@@ -24,8 +24,6 @@ fn main() {
     let mut value_a = 45;
     let mut value_b = 66;
     // Let's swap these two!
-    // value_a = value_b;
-    // value_b = value_a;
     mem::swap(&mut value_a, &mut value_b);
     println!("value a: {}; value b: {}", value_a, value_b);
 }
