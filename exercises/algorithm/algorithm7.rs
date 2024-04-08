b1f7044c2a07ef84196b149799f07fe0a559cd4f
Andrew211vibe <1207368051@qq.com>
2024-04-09 02:59:17 +0800

    done algorithm7

--- exercises/algorithm/algorithm7.rs
+++ exercises/algorithm/algorithm7.rs
@@ -3,7 +3,7 @@
 	This question requires you to use a stack to achieve a bracket match
 */
 
-// I AM NOT DONE
+
 #[derive(Debug)]
 struct Stack<T> {
 	size: usize,
@@ -31,8 +31,11 @@ impl<T> Stack<T> {
 		self.size += 1;
 	}
 	fn pop(&mut self) -> Option<T> {
-		// TODO
-		None
+		if self.len() == 0 {
+			return None
+		}
+		self.size -= 1;
+		self.data.pop()
 	}
 	fn peek(&self) -> Option<&T> {
 		if 0 == self.size {
@@ -101,8 +104,33 @@ impl<'a, T> Iterator for IterMut<'a, T> {
 
 fn bracket_match(bracket: &str) -> bool
 {
-	//TODO
-	true
+	let mut stack: Stack<char> = Stack::new();
+	for s in bracket.chars() {
+		dbg!("{:?} {:?}", stack.len(), s);
+		match s {
+			'(' | '{' | '[' => { stack.push(s); },
+			'}' => { 
+				if (stack.is_empty()) { return false; }
+				if let Some(i) = stack.pop() {
+					if i != '{' { return false }
+				} else { return false; }
+			},
+			']' => { 
+				if (stack.is_empty()) { return false; }
+				if let Some(i) = stack.pop() {
+					if i != '[' { return false; }
+				}
+			},
+			')' => { 
+				if (stack.is_empty()) { return false; }
+				if let Some(i) = stack.pop() {
+					if i != '(' { return false; }
+				}
+			},
+			_ => continue,
+		}
+	}
+	stack.is_empty()
 }
 
 #[cfg(test)]
