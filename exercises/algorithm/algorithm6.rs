ef874374d29eb8f727a1fc5d191d2d44675e0c1a
Andrew211vibe <1207368051@qq.com>
2024-04-09 02:01:57 +0800

    done algorithm6

--- exercises/algorithm/algorithm6.rs
+++ exercises/algorithm/algorithm6.rs
@@ -3,7 +3,7 @@
 	This problem requires you to implement a basic DFS traversal
 */
 
-// I AM NOT DONE
+
 use std::collections::HashSet;
 
 struct Graph {
@@ -23,13 +23,21 @@ impl Graph {
     }
 
     fn dfs_util(&self, v: usize, visited: &mut HashSet<usize>, visit_order: &mut Vec<usize>) {
-        //TODO
+        for child in &self.adj[v] {
+            if !visited.contains(&child) { // 未访问
+                visited.insert(*child);
+                visit_order.push(*child);
+                self.dfs_util(*child, visited, visit_order);
+            }
+        }
     }
 
     // Perform a depth-first search on the graph, return the order of visited nodes
     fn dfs(&self, start: usize) -> Vec<usize> {
         let mut visited = HashSet::new();
         let mut visit_order = Vec::new(); 
+        visited.insert(start);
+        visit_order.push(start);
         self.dfs_util(start, &mut visited, &mut visit_order);
         visit_order
     }
